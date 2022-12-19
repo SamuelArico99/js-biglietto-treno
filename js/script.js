c869b4769600c0d67f09d12f1af296b24e38ad2d
SamuelArico99 <samuel16699.sa@gmail.com>
2022-12-19 17:26:51 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -6,18 +6,27 @@ console.log(numerochilometri);
 console.log(typeof numerochilometri);
 
 // Chiedi l' età del passeggero
-const età = prompt('Quanti anni ha?');
-console.log(età);
-console.log(typeof età);
+const eta = prompt('Quanti anni ha?');
+console.log(eta);
+console.log(typeof eta);
 // Calcolo prezzo totale in base ai chilometri (0.21 € al km)
 const prezzo = 0.21;
 let prezzofinale = prezzo * parseInt(numerochilometri);
-console.log(prezzofinale);
-// Se il cliente è minorenne va applicato uno sconto del 20%
-if(età >= 18){
-    
-}
-// Se il cliente è over 65 va applicato uno sconto del 40%
+console.log(prezzofinale.toFixed(2));
+document.getElementById('info').innerHTML = prezzofinale.toFixed(2);
 
+// Se il cliente è minorenne va applicato uno sconto del 20%
+if(eta < 18){
+    prezzofinale = (prezzofinale -(prezzofinale / 100)*20);
+    console.log(prezzofinale.toFixed(2));
+    document.getElementById('info').innerHTML = prezzofinale.toFixed(2);
+}
+
+// Se il cliente è over 65 va applicato uno sconto del 40%
+else if(eta >= 65){
+    prezzofinale = (prezzofinale -(prezzofinale / 100)*40);
+    console.log(prezzofinale.toFixed(2));
+    document.getElementById('info').innerHTML = prezzofinale.toFixed(2);
+}
 // Output del prezzo con massimo due decimali
 // Fine
